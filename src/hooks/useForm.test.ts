bbb718133343826ba4b817cb05165666045a239a
luke-h1 <luke.howsam@yahoo.com>
2024-03-15 15:03:11 +0000

    chore: add tests

--- src/hooks/useForm.test.ts
+++ src/hooks/useForm.test.ts
@@ -1,7 +1,13 @@
 import { renderHook, act } from "@testing-library/react-hooks";
 import { useForm } from "./useForm";
+import { ChangeEvent } from "react";
 
 describe("useForm", () => {
+  const getFakeTestEvent = (value: any = "") =>
+    ({
+      preventDefault: jest.fn(),
+      target: { value },
+    } as unknown as ChangeEvent<any>);
   it("should handle input change", () => {
     const { result } = renderHook(() =>
       useForm({
@@ -11,8 +17,9 @@ describe("useForm", () => {
 
     act(() => {
       return result.current.handleChange("test")({
+        preventDefault: jest.fn(),
         target: { value: "new value" },
-      } as React.ChangeEvent<HTMLInputElement>);
+      } as unknown as ChangeEvent<any>);
     });
 
     expect(result.current.data).toEqual({ test: "new value" });
@@ -36,7 +43,19 @@ describe("useForm", () => {
     expect(onSubmit).toHaveBeenCalled();
   });
 
-  it("should handle form submission with validations", () => {
+  it("should initialize defaultValues", () => {
+    const { result } = renderHook(() =>
+      useForm({
+        initialValues: {
+          name: "John",
+        },
+      })
+    );
+
+    expect(result.current.data.name).toBe("John");
+  });
+
+  it("should not submit for if validation errors", () => {
     const onSubmit = jest.fn();
     const { result } = renderHook(() =>
       useForm({
@@ -115,5 +134,48 @@ describe("useForm", () => {
 
     expect(result.current.errors).toEqual({ test: "Not test" });
     expect(onSubmit).not.toHaveBeenCalled();
+  });
+
+  it("should reset errors on submit", () => {
+    const validationMessage = "The minimum length is 7 characters.";
+    const onSubmit = jest.fn();
+    const { result } = renderHook(() =>
+      useForm({
+        validations: {
+          name: {
+            custom: {
+              isValid: (value) => value?.length > 6,
+              message: validationMessage,
+            },
+          },
+        },
+        onSubmit,
+      })
+    );
+
+    // Name is too short
+    onSubmit.mockReset();
+    act(() => {
+      result.current.handleChange("name")(getFakeTestEvent("123"));
+    });
+
+    act(() => {
+      result.current.handleSubmit(getFakeTestEvent());
+    });
+
+    expect(onSubmit).toHaveBeenCalledTimes(0);
+    expect(result.current.errors.name).toBe(validationMessage);
+
+    // Name is long enough
+    act(() => {
+      result.current.handleChange("name")(getFakeTestEvent("Felix123"));
+    });
+
+    act(() => {
+      result.current.handleSubmit(getFakeTestEvent());
+    });
+
+    expect(onSubmit).toHaveBeenCalledTimes(1);
+    expect(result.current.errors.name).toBeUndefined();
   });
 });
